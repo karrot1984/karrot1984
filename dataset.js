fd7c0fcfb5b5d1d24d468fc0a453b71f3b84e6bf
karrot1984 <56506160+karrot1984@users.noreply.github.com>
2019-10-13 18:52:14 +0200

    Create dataset.js

---
+++ dataset.js
@@ -0,0 +1,554 @@
+[
+    {
+      "cras_año": 2011,
+      "cras_animal_numero": 90,
+      "cras_animal_grupo_taxonomico": "Mamíferos",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2011,
+      "cras_animal_numero": 256,
+      "cras_animal_grupo_taxonomico": "Aves rapaces",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2011,
+      "cras_animal_numero": 1943,
+      "cras_animal_grupo_taxonomico": "Otras aves",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2011,
+      "cras_animal_numero": 104,
+      "cras_animal_grupo_taxonomico": "Reptiles",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2011,
+      "cras_animal_numero": 0,
+      "cras_animal_grupo_taxonomico": "Peces",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2011,
+      "cras_animal_numero": 6,
+      "cras_animal_grupo_taxonomico": "Anfibios",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2011,
+      "cras_animal_numero": 1,
+      "cras_animal_grupo_taxonomico": "Invertebrados",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2011,
+      "cras_animal_numero": 70,
+      "cras_animal_grupo_taxonomico": "Mamíferos",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2011,
+      "cras_animal_numero": 11,
+      "cras_animal_grupo_taxonomico": "Aves rapaces",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2011,
+      "cras_animal_numero": 27,
+      "cras_animal_grupo_taxonomico": "Otras aves",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2011,
+      "cras_animal_numero": 450,
+      "cras_animal_grupo_taxonomico": "Reptiles",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2011,
+      "cras_animal_numero": 2,
+      "cras_animal_grupo_taxonomico": "Anfibios",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2012,
+      "cras_animal_numero": 169,
+      "cras_animal_grupo_taxonomico": "Mamíferos",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2012,
+      "cras_animal_numero": 317,
+      "cras_animal_grupo_taxonomico": "Aves rapaces",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2012,
+      "cras_animal_numero": 788,
+      "cras_animal_grupo_taxonomico": "Otras aves",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2012,
+      "cras_animal_numero": 52,
+      "cras_animal_grupo_taxonomico": "Reptiles",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2012,
+      "cras_animal_numero": 51,
+      "cras_animal_grupo_taxonomico": "Peces",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2012,
+      "cras_animal_numero": 1,
+      "cras_animal_grupo_taxonomico": "Anfibios",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2012,
+      "cras_animal_numero": 274,
+      "cras_animal_grupo_taxonomico": "Mamíferos",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2012,
+      "cras_animal_numero": 95,
+      "cras_animal_grupo_taxonomico": "Otras aves",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2012,
+      "cras_animal_numero": 493,
+      "cras_animal_grupo_taxonomico": "Reptiles",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2012,
+      "cras_animal_numero": 1,
+      "cras_animal_grupo_taxonomico": "Peces",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2012,
+      "cras_animal_numero": 104,
+      "cras_animal_grupo_taxonomico": "Invertebrados",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2012,
+      "cras_animal_numero": 22,
+      "cras_animal_grupo_taxonomico": "Aves rapaces",
+      "cras_animal_tipo": "Ave de cetrería"
+    },
+    {
+      "cras_año": 2013,
+      "cras_animal_numero": 232,
+      "cras_animal_grupo_taxonomico": "Mamíferos",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2013,
+      "cras_animal_numero": 297,
+      "cras_animal_grupo_taxonomico": "Aves rapaces",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2013,
+      "cras_animal_numero": 1032,
+      "cras_animal_grupo_taxonomico": "Otras aves",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2013,
+      "cras_animal_numero": 70,
+      "cras_animal_grupo_taxonomico": "Reptiles",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2013,
+      "cras_animal_numero": 4,
+      "cras_animal_grupo_taxonomico": "Peces",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2013,
+      "cras_animal_numero": 3,
+      "cras_animal_grupo_taxonomico": "Anfibios",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2013,
+      "cras_animal_numero": 1,
+      "cras_animal_grupo_taxonomico": "Insectos",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2013,
+      "cras_animal_numero": 2,
+      "cras_animal_grupo_taxonomico": "Otros invertebrados",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2013,
+      "cras_animal_numero": 205,
+      "cras_animal_grupo_taxonomico": "Mamíferos",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2013,
+      "cras_animal_numero": 110,
+      "cras_animal_grupo_taxonomico": "Otras aves",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2013,
+      "cras_animal_numero": 701,
+      "cras_animal_grupo_taxonomico": "Reptiles",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2013,
+      "cras_animal_numero": 4,
+      "cras_animal_grupo_taxonomico": "Anfibios",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2013,
+      "cras_animal_numero": 17,
+      "cras_animal_grupo_taxonomico": "Otros invertebrados",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2013,
+      "cras_animal_numero": 13,
+      "cras_animal_grupo_taxonomico": "Aves rapaces",
+      "cras_animal_tipo": "Ave de cetrería"
+    },
+    {
+      "cras_año": 2014,
+      "cras_animal_numero": 362,
+      "cras_animal_grupo_taxonomico": "Mamíferos",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2014,
+      "cras_animal_numero": 321,
+      "cras_animal_grupo_taxonomico": "Aves rapaces",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2014,
+      "cras_animal_numero": 1006,
+      "cras_animal_grupo_taxonomico": "Otras aves",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2014,
+      "cras_animal_numero": 65,
+      "cras_animal_grupo_taxonomico": "Reptiles",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2014,
+      "cras_animal_numero": 7,
+      "cras_animal_grupo_taxonomico": "Peces",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2014,
+      "cras_animal_numero": 5,
+      "cras_animal_grupo_taxonomico": "Anfibios",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2014,
+      "cras_animal_numero": 1,
+      "cras_animal_grupo_taxonomico": "Invertebrados",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2014,
+      "cras_animal_numero": 96,
+      "cras_animal_grupo_taxonomico": "Mamíferos",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2014,
+      "cras_animal_numero": 182,
+      "cras_animal_grupo_taxonomico": "Otras aves",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2014,
+      "cras_animal_numero": 687,
+      "cras_animal_grupo_taxonomico": "Reptiles",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2014,
+      "cras_animal_numero": 2,
+      "cras_animal_grupo_taxonomico": "Anfibios",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2014,
+      "cras_animal_numero": 3,
+      "cras_animal_grupo_taxonomico": "Invertebrados",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2014,
+      "cras_animal_numero": 12,
+      "cras_animal_grupo_taxonomico": "Aves rapaces",
+      "cras_animal_tipo": "Ave de cetrería"
+    },
+    {
+      "cras_año": 2015,
+      "cras_animal_numero": 825,
+      "cras_animal_grupo_taxonomico": "Mamíferos",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2015,
+      "cras_animal_numero": 398,
+      "cras_animal_grupo_taxonomico": "Aves rapaces",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2015,
+      "cras_animal_numero": 1693,
+      "cras_animal_grupo_taxonomico": "Otras aves",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2015,
+      "cras_animal_numero": 88,
+      "cras_animal_grupo_taxonomico": "Reptiles",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2015,
+      "cras_animal_numero": 16,
+      "cras_animal_grupo_taxonomico": "Peces",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2015,
+      "cras_animal_numero": 2,
+      "cras_animal_grupo_taxonomico": "Anfibios",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2015,
+      "cras_animal_numero": 3,
+      "cras_animal_grupo_taxonomico": "Invertebrados",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2015,
+      "cras_animal_numero": 152,
+      "cras_animal_grupo_taxonomico": "Mamíferos",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2015,
+      "cras_animal_numero": 1029,
+      "cras_animal_grupo_taxonomico": "Otras aves",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2015,
+      "cras_animal_numero": 536,
+      "cras_animal_grupo_taxonomico": "Reptiles",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2015,
+      "cras_animal_numero": 3,
+      "cras_animal_grupo_taxonomico": "Peces",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2015,
+      "cras_animal_numero": 1,
+      "cras_animal_grupo_taxonomico": "Anfibios",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2015,
+      "cras_animal_numero": 8,
+      "cras_animal_grupo_taxonomico": "Invertebrados",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2015,
+      "cras_animal_numero": 27,
+      "cras_animal_grupo_taxonomico": "Aves rapaces",
+      "cras_animal_tipo": "Ave de cetrería"
+    },
+    {
+      "cras_año": 2015,
+      "cras_animal_numero": 6,
+      "cras_animal_grupo_taxonomico": "Otras aves",
+      "cras_animal_tipo": "Ave de cetrería"
+    },
+    {
+      "cras_año": 2016,
+      "cras_animal_numero": 862,
+      "cras_animal_grupo_taxonomico": "Mamíferos",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2016,
+      "cras_animal_numero": 364,
+      "cras_animal_grupo_taxonomico": "Aves rapaces",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2016,
+      "cras_animal_numero": 1075,
+      "cras_animal_grupo_taxonomico": "Otras aves",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2016,
+      "cras_animal_numero": 58,
+      "cras_animal_grupo_taxonomico": "Reptiles",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2016,
+      "cras_animal_numero": 5,
+      "cras_animal_grupo_taxonomico": "Peces",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2016,
+      "cras_animal_numero": 7,
+      "cras_animal_grupo_taxonomico": "Invertebrados",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2016,
+      "cras_animal_numero": 141,
+      "cras_animal_grupo_taxonomico": "Mamíferos",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2016,
+      "cras_animal_numero": 231,
+      "cras_animal_grupo_taxonomico": "Otras aves",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2016,
+      "cras_animal_numero": 427,
+      "cras_animal_grupo_taxonomico": "Reptiles",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2016,
+      "cras_animal_numero": 3,
+      "cras_animal_grupo_taxonomico": "Peces",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2016,
+      "cras_animal_numero": 1,
+      "cras_animal_grupo_taxonomico": "Anfibios",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2016,
+      "cras_animal_numero": 95,
+      "cras_animal_grupo_taxonomico": "Invertebrados",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2016,
+      "cras_animal_numero": 21,
+      "cras_animal_grupo_taxonomico": "Aves rapaces",
+      "cras_animal_tipo": "Ave de cetrería"
+    },
+    {
+      "cras_año": 2017,
+      "cras_animal_numero": 362,
+      "cras_animal_grupo_taxonomico": "Mamíferos",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2017,
+      "cras_animal_numero": 357,
+      "cras_animal_grupo_taxonomico": "Aves rapaces",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2017,
+      "cras_animal_numero": 1333,
+      "cras_animal_grupo_taxonomico": "Otras aves",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2017,
+      "cras_animal_numero": 126,
+      "cras_animal_grupo_taxonomico": "Reptiles",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2017,
+      "cras_animal_numero": 1,
+      "cras_animal_grupo_taxonomico": "Anfibios",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2017,
+      "cras_animal_numero": 1,
+      "cras_animal_grupo_taxonomico": "Invertebrados",
+      "cras_animal_tipo": "Autóctono"
+    },
+    {
+      "cras_año": 2017,
+      "cras_animal_numero": 113,
+      "cras_animal_grupo_taxonomico": "Mamíferos",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2017,
+      "cras_animal_numero": 201,
+      "cras_animal_grupo_taxonomico": "Otras aves",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2017,
+      "cras_animal_numero": 490,
+      "cras_animal_grupo_taxonomico": "Reptiles",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2017,
+      "cras_animal_numero": 3,
+      "cras_animal_grupo_taxonomico": "Peces",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2017,
+      "cras_animal_numero": 1,
+      "cras_animal_grupo_taxonomico": "Anfibios",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2017,
+      "cras_animal_numero": 56,
+      "cras_animal_grupo_taxonomico": "Invertebrados",
+      "cras_animal_tipo": "Alóctono"
+    },
+    {
+      "cras_año": 2017,
+      "cras_animal_numero": 14,
+      "cras_animal_grupo_taxonomico": "Aves rapaces",
+      "cras_animal_tipo": "Ave de cetrería"
+    }
+  ]
